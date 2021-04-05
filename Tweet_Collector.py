b662cfc35065361a9e6f087cb9497bc1d563c5be
ritik <kitri@DESKTOP-GNPDQT7>
2021-04-05 14:58:58 -0400

    Tested Twitter API and fetched some tweets.

---
+++ Tweet_Collector.py
@@ -0,0 +1,44 @@
+#getting started 
+import tweepy as tw
+import pandas as pd
+import os
+
+#insert keys and secret tokens
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret= ""
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+search_words = "#disneyplus"
+date_since = "2021-01-01"
+
+#fetching tweets
+
+tweets = tw.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(100)
+
+#filtering retweets
+new_search = search_words + " -filter:retweets"
+new_search
+
+list_tweets = [tweet.text for tweet in tweets]
+
+#print(list_tweets)
+
+tweets = tw.Cursor(api.search, 
+                           q=new_search,
+                           lang="en",
+                           since=date_since).items(100)
+users_locs = [[tweet.user.screen_name, tweet.user.location,tweet.text] for tweet in tweets]
+
+# Dataframe -test
+tweet_df = pd.DataFrame(data=users_locs, 
+                    columns=['user', "location","tweet"])
+print(tweet_df)
+
