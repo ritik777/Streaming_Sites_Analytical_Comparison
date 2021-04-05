68df0da8af84365b0118a8c29fbf07ebd8bf5721
ritik <kitri@DESKTOP-GNPDQT7>
2021-04-05 16:23:28 -0400

    tested db

---
+++ db_connector.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE streaming_analytics_db")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
